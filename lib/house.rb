7447bb148f4e8585357199e67bb78328d5534114
Zach Stearns <zach.stearns92@gmail.com>
2020-08-26 09:45:53 -0600

    Add add room test

--- lib/house.rb
+++ lib/house.rb
@@ -1,1 +1,18 @@
- 
+class House
+  attr_reader :address, :rooms
+
+  def initialize(price, address)
+    @price = price
+    @address = address
+    @rooms = []
+  end
+
+  def price
+    @price.slice!(0)
+    @price.to_i
+  end
+
+  def add_room(room)
+    @rooms << room
+  end
+end
