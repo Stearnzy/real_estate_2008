a5b6d4597ecdfe5a79391fe5d2b046191963aad9
Zach Stearns <zach.stearns92@gmail.com>
2020-08-26 10:20:48 -0600

    Add details test

--- test/house_test.rb
+++ test/house_test.rb
@@ -71,4 +71,9 @@ class HouseTest < Minitest::Test
     house.add_room(room_4)
     assert_equal 1900, house.area
   end
+
+  def test_display_house_details
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal ({"price" => 400000, "address" => "123 sugar lane"}), house.details
+  end
 end
