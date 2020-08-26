06f8ab166adbecc5587ede99c010058c53c54d21
Zach Stearns <zach.stearns92@gmail.com>
2020-08-26 10:52:24 -0600

    Add sort rooms by category test

--- test/house_test.rb
+++ test/house_test.rb
@@ -102,4 +102,17 @@ class HouseTest < Minitest::Test
     house.add_room(room_3)
     assert_equal [room_1, room_2, room_3, room_4], house.rooms_sorted_by_area
   end
+
+  def test_sort_rooms_by_category
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_4)
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    assert_equal ({:bedroom => [room_1, room_2], :living_room => [room_3], :basement => [room_4]}), house.rooms_by_category
+  end
 end
