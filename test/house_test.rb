239a8ac316bf4fb1fcbe3a83f5b3b77412b550ee
Zach Stearns <zach.stearns92@gmail.com>
2020-08-26 09:43:22 -0600

    Add readable tests, no rooms to start test, rooms can be added test

--- test/house_test.rb
+++ test/house_test.rb
@@ -5,4 +5,34 @@ require './lib/house'
 
 class HouseTest < Minitest::Test
 
+  def test_it_exists
+    house = House.new("$400000", "123 sugar lane")
+    assert_instance_of House, house
+  end
+
+  def test_price
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal 400000, house.price
+  end
+
+  def test_readable_address
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal "123 sugar lane", house.address
+  end
+
+  def test_house_has_no_rooms_to_start
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal [], house.rooms
+  end
+
+  def test_rooms_can_be_added
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal [], house.rooms
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    assert_equal [room_1, room_2], house.rooms
+  end
+
 end
